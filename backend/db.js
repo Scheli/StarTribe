87c36533239cc5ee12aad930f2d655710c72a2cf
Scheli <nicoloandreola50@gmail.com>
2025-07-14 11:08:22 +0200

    Inserimento database
    inizializzazione database con connesione mongodb utilizzando nodejs

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+let db;
+
+export async function connectToDB() {
+  if (db) return db; 
+
+  try {
+    await client.connect();
+    db = client.db("StarTribeDB");  
+    console.log("Connessione a MongoDB stabilita");
+    return db;
+  } catch (e) {
+    console.error("Errore di connessione:", e);
+    throw e;
+  }
+}
+
