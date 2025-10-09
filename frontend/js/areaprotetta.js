675db64de20c0626b313eb3be3ddda66e334e185
FrancescoDelColombo <allievo_delcolombo.f@itsumbria.it>
2025-10-09 14:01:38 +0200

    Fix popup e profilo

--- frontend/js/areaprotetta.js
+++ frontend/js/areaprotetta.js
@@ -3,7 +3,7 @@ import { verificaAccesso } from "./verificaAccesso.js";
 async function init() {
   const result = await verificaAccesso();
   if (result.accesso) {
-    document.getElementById("welcome").textContent = result.message;
+    document.getElementById("welcome").value = result.message;
   } else {
     alert(result.message);
     window.location.href = "/frontend/html/registrazione.html";
